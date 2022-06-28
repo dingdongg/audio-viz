8c36e7877b95a4da2acfdcb1148d9dc10c8cd776
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-06-28 15:56:12 -0700

    Allow users to select local audio files

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
+const file = document.getElementById('file-upload');
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -52,15 +54,67 @@ container.addEventListener('click', () => {
         // = decibel value of each frequency
         analyzer.getByteFrequencyData(dataArray);
 
-        for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
-            context.fillStyle = 'white';
-            context.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-            x += barWidth;
-        }
+        drawlVisualizer(bufferLength, x, barWidth, barHeight, dataArray)
 
         requestAnimationFrame(animate);
     }
     animate();
 });
 
+file.addEventListener('change', function() {
+    const files = this.files;
+    const audio1 = document.getElementById('audio1');
+    audio1.src = URL.createObjectURL(files[0]);
+    audio1.load();
+    audio1.play();
+
+    audioSource = audioContext.createMediaElementSource(audio1);
+
+    // create analyzer node (get time/frequency data)
+    analyzer = audioContext.createAnalyser();
+
+    // allow analyzer to analyze our audio node
+    audioSource.connect(analyzer);
+
+    // connect analyzer to default speakers
+    analyzer.connect(audioContext.destination);
+
+    // number of audio samples wanted in analyzer data file
+    analyzer.fftSize = 64;
+
+    // # data values in analyzer data file
+    // HALF of fftSize 
+    // = number of bars on screen
+    const bufferLength = analyzer.frequencyBinCount;
+
+    const dataArray = new Uint8Array(bufferLength);
+
+    // width of single bar in visualizer
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x; // increment by barWidth for each bar
+
+    function animate() {
+        x = 0;
+        // clear canvas first
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // determines the height of each bar 
+        // = decibel value of each frequency
+        analyzer.getByteFrequencyData(dataArray);
+
+        drawlVisualizer(bufferLength, x, barWidth, barHeight, dataArray)
+
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
+
+function drawlVisualizer(bufferLength, x, barWidth, barHeight, dataArray) {
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        context.fillStyle = 'white';
+        context.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth;
+    }
+}
